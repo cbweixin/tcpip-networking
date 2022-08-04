7179f545c2198b178f5dee51670b3e17546617f4
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2022-08-04 12:12:45 -0700

    add some comments

--- hello-server/inet_ntoa.c
+++ hello-server/inet_ntoa.c
@@ -13,14 +13,19 @@ int main(int argc, char *argv[]) {
     addr1.sin_addr.s_addr = htonl(0x1020304);
     addr2.sin_addr.s_addr = htonl(0x1010101);
 
+    // notice, this function would return a pointer
+    // after you got this pointer, you need to copy the value to your own address
+    // otherwise, after next call, the pointer would be overwrite with new value
+    // str_ptr now has value 1.2.3.4
     str_ptr = inet_ntoa(addr1.sin_addr);
     strcpy(str_arr, str_ptr);
     printf("Dotted-Decimal notation1: %s \n", str_ptr);
 
+    // call again, now str_ptr changed to 1.1.1.1
     inet_ntoa(addr2.sin_addr);
     printf("Dotted-Decimal notation2: %s \n", str_ptr);
+    // str_arr still 1.2.3.4
     printf("Dotted-Decimal notation3: %s \n", str_arr);
 
     return 0;
-
 }
