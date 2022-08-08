0b0ef19886f4d0e90116732dbecd1d5f739bd9da
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2022-08-08 15:08:47 -0700

    local variable also doable

--- calc-server/send_3times.c
+++ calc-server/send_3times.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[]) {
     char msg3[] = "Nice to meet you.";
     char *str_arr[] = {msg1, msg2, msg3};
     char read_buf[100];
-    int str_len;
+//    int str_len;
 
     char message[] = "hello world!";
     if (argc != 2) {
@@ -68,7 +68,7 @@ int main(int argc, char *argv[]) {
 
     int i;
     for (i = 0; i < 3; i++) {
-        str_len = strlen(str_arr[i]) + 1;
+        size_t str_len = strlen(str_arr[i]) + 1;
         write(client_sock, (char *) (&str_len), 4);
         write(client_sock, str_arr[i], str_len);
 
