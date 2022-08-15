52debfb2e76c64f0b7a97e4ed2788e0108fc65db
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2022-08-15 13:12:39 -0700

    add new example

---
+++ chapt-10-multiple-process/remove_zoombie.c
@@ -0,0 +1,50 @@
+//
+// Created by wei,xin on 8/15/22.
+//
+
+#include <sys/wait.h>
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void read_childproc(int sig) {
+    int status;
+    pid_t id = waitpid(-1, &status, WNOHANG);
+    if (WIFEXITED(status)) {
+        printf("Removed proc id: %d \n", id);
+        printf("Child send: %d \n", WEXITSTATUS(status));
+    }
+}
+
+int main(int argc, char *argv[]) {
+    pid_t pid;
+    struct sigaction act;
+    act.sa_handler = read_childproc;
+    sigemptyset(&act.sa_mask);
+    sigaction(SIGCHLD, &act, 0);
+
+    pid = fork();
+    if (pid == 0) { // child proc execution
+        puts("Hi! I'm child process");
+        sleep(10);
+        return 12;
+    } else { // prarent proc execution
+        printf("Child proc id: %d \n", pid);
+        pid = fork();
+        if (pid == 0) { // another child proc execution
+            puts("Hi! I'm another child process");
+            sleep(10);
+            exit(33);
+        } else {
+            int i;
+            printf("Child proc id: %d \n", pid);
+            for (i = 0; i < 5; i++) {
+                puts("wait...");
+                sleep(5);
+            }
+
+        }
+    }
+    return 0;
+}
