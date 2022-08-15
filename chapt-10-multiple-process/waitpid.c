b5cbf38b5ccd7817c95f64ca6b74d204cc6e1d0d
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2022-08-15 11:41:59 -0700

    add new examples

---
+++ chapt-10-multiple-process/waitpid.c
@@ -0,0 +1,26 @@
+//
+// Created by wei,xin on 8/15/22.
+//
+
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    int status;
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        sleep(15);
+        return 24;
+    } else {
+        while (!waitpid(-1, &status, WNOHANG)) {
+            sleep(3);
+            puts("sleep 3sec.");
+        }
+    }
+    if (WIFEXITED(status)) {
+        printf("Child send %d \n", WEXITSTATUS(status));
+    }
+    return 0;
+}
