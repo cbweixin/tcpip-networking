13ce43d5616d46182b29c8ed64a115afde890d70
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2022-08-16 13:18:45 -0700

    ❯ ./p3.out Parent proc output: Who are you? Child proc output: Thank you for your message ❯ gcc pipe3.c -o p3.out

--- chapt-11-IPC/pipe3.c
+++ chapt-11-IPC/pipe3.c
@@ -8,25 +8,27 @@
 #define BUF_SIZE 30
 
 int main(int argc, char *argv[]) {
-    int fds[2];
+    int fds1[2], fds2[2];
     char str1[] = "Who are you?";
     char str2[] = "Thank you for your message";
     char buf[BUF_SIZE];
     pid_t pid;
 
-    pipe(fds);
+    // using 2 pipe
+    pipe(fds1);
+    pipe(fds2);
+
     pid = fork();
     if (pid == 0) { // child proc bidirection communication
-        write(fds[1], str1, sizeof(str1));
+        write(fds1[1], str1, sizeof(str1));
         // need sleep , otherwise, read would get the data write by itsef , then parent proc would waiting forever
-        sleep(2);
-        read(fds[0], buf, BUF_SIZE);
+        read(fds2[0], buf, BUF_SIZE);
         printf("Child proc output: %s \n", buf);
     } else {  // parent proc bidirectional communication
-        read(fds[0], buf, BUF_SIZE);
+        read(fds1[0], buf, BUF_SIZE);
         printf("Parent proc output: %s \n", buf);
-        write(fds[1], str2, sizeof(str2));
-        sleep(3);
+        write(fds2[1], str2, sizeof(str2));
+//        sleep(3);
     }
 
     return 0;
