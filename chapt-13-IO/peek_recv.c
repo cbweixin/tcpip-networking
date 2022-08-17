b0ac35b756a0021647c656188d456185976d995d
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2022-08-17 14:21:10 -0700

    ❯ ./or.out 19999 Connected client 1 message from client 123 sizeof(buf) : 3 Urgent message: 0 sizeof(buf) : 3 Urgent message: 0 message from client 456 message from client 789

--- chapt-13-IO/peek_recv.c
+++ chapt-13-IO/peek_recv.c
@@ -2,7 +2,6 @@
 // Created by wei,xin on 8/17/22.
 //
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,42 +10,36 @@
 #include <signal.h>
 #include <fcntl.h>
 
-#define BUF_SIZE 3
+#define BUF_SIZE 30
 
 void error_handling(char *message);
 
-void urg_handler(int signo);
-
-int acpt_sock, recv_sock;
 
 int main(int argc, char *argv[]) {
+    int acpt_sock, recv_sock;
     char message[BUF_SIZE];
     int str_len, i = 0, state;
 
-    struct sockaddr_in serv_adr, recv_adr;
+    struct sockaddr_in serv_adr, acpt_adr;
     socklen_t serv_adr_sz;
-    struct sigaction act;
 
     if (argc != 2) {
         printf("Usage : %s <port> \n", argv[0]);
         exit(1);
     }
 
-    act.sa_handler = urg_handler;
-    sigemptyset(&act.sa_mask);
-    act.sa_flags = 0;
 
     acpt_sock = socket(PF_INET, SOCK_STREAM, 0);
     if (acpt_sock == -1) {
         error_handling("socket() error");
     }
 
-    memset(&recv_adr, 0, sizeof(recv_adr));
-    recv_adr.sin_family = AF_INET;
-    recv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
-    recv_adr.sin_port = htons(atoi(argv[1]));
+    memset(&acpt_adr, 0, sizeof(acpt_adr));
+    acpt_adr.sin_family = AF_INET;
+    acpt_adr.sin_addr.s_addr = htonl(INADDR_ANY);
+    acpt_adr.sin_port = htons(atoi(argv[1]));
 
-    if (bind(acpt_sock, (struct sockaddr *) &recv_adr, sizeof(recv_adr)) == -1) {
+    if (bind(acpt_sock, (struct sockaddr *) &acpt_adr, sizeof(acpt_adr)) == -1) {
         error_handling("bind() error");
     }
     if (listen(acpt_sock, 5) == -1) {
@@ -83,14 +76,3 @@ void error_handling(char *message) {
     exit(1);
 }
 
-void urg_handler(int signo) {
-    int str_len;
-    char buf[BUF_SIZE];
-    // TODO: why need -1?
-    str_len = recv(recv_sock, buf, sizeof(buf) - 1, MSG_OOB);
-//    str_len = recv(recv_sock, buf, 1, MSG_OOB);
-    printf("sizeof(buf) : %lu \n", sizeof(buf));
-//    str_len = recv(recv_sock, buf, sizeof(buf), MSG_OOB);
-    buf[str_len] = 0;
-    printf("Urgent message: %s \n", buf);
-}
